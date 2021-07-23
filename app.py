4d3d6dceb6129e5ec669d950b09657397019e6ef
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-07-23 12:26:36 +0530

    update page added and update route added

--- app.py
+++ app.py
@@ -50,7 +50,43 @@ def insert():
     # return "Names added Successfully!"
     return redirect(url_for("admin"))
 
-    
+@app.route('/update',methods=["GET","POST"])
+def update():
+    val = []
+    for x in collection.find({},{"_id" : 0 , "Name" : 1}):
+        y = x["Name"]
+        val.append(y)
+    val2 = len(val)
+    for i in range(0,val2):
+        ln = val[i]
+        query = {"Name" : ln}
+        newvalues = { "$set": { 'id' : i }}
+        collection.update_one(query , newvalues)
+    val = []
+    for x in collection.find({},{"id" : 1 , "Name" : 1}):
+        y = x["Name"]
+        val.append(y)
+    val2 = []
+    for z in collection.find({},{'id' : 1 , "Name " : 1}):
+        a = x['id']
+        val2.append(a)
+    val3 = len(val2)
+
+
+    return render_template("update.html",val3 = val3 ,val =val)
+
+@app.route('/update-view',methods=["GET","POST"])
+def update_view():
+    # num1 = int(request.form["num1"])
+    num1 = int(request.values.get("num1"))
+    # print(num1)
+    updt = str(request.values.get("upt"))
+
+    query = {'id' : num1}
+    newvalues = { "$set" : {"Name" : updt}}
+    collection.update_one(query, newvalues)
+    # return redirect(url_for("admin"))
+    return redirect(request.referrer)
 
 
 
